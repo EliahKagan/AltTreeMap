e1458317f80a09677ae23c506cb9e3c9d793dc51
Eliah Kagan <degeneracypressure@gmail.com>
2020-03-14 03:42:51 -0400

    rewokred primes script for automated use

--- primes.rb
+++ primes.rb
@@ -1,5 +1,13 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
+$VERBOSE = true
 
 require 'prime'
 
-puts Prime.take_while { |prime| prime <= 10_000_000 } * ', '
+if ARGV.length != 1
+  raise ARGV.empty? ? 'too few arguments' : 'too many arguments'
+end
+
+upper_bound = Integer(ARGV[0].to_i)
+puts Prime.take_while { |prime| prime <= upper_bound }.join(' ')
